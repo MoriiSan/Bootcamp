46ab873d8937f676f11899d632726a7bd299c2f6
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2023-12-28 05:45:50 +0800

    react bootcamp DONE

--- projecttwo/mrtpay/src/Bank.tsx
+++ projecttwo/mrtpay/src/Bank.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Bank.css';
+import Navigation from './components/Navigation';
  
 const Bank = () => {
+    // const [name, setName] = useState("John Doe");
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [tempFname, setTempFname] = useState("");
+    const [tempLname, setTempLname] = useState("");
+
+    const clickEvent = () => {
+        console.log("Clicked");
+        setTempFname(firstName);
+        setTempLname(lastName);
+    }
+
+    // const changeNameHandler = (e: React.ChangeEvent<HTMLInputElement>)=> {
+    //     setFirstName(e.target.value);
+    // }
+
     return (
         <div>
-            <h1> WELCOME!
-            </h1>
-            <input placeholder='First Name'/>
-            <button >Submit</button>
+            <Navigation/>
+
+            <h1> WELCOME, {tempFname} {tempLname}!</h1>
+
+            <input onChange={(e)=> setFirstName(e.target.value)}
+            placeholder='First Name'/>
+
+            <input onChange={(e)=> setLastName(e.target.value)}
+            placeholder='Last Name' />
+
+            <button onClick={clickEvent}>
+                Submit
+            </button>
+
+            <div>Footer</div>
         </div>
     )
 }
