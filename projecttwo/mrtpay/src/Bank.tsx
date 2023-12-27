ab67cf330cd7b217c529d956c9e971f83798e765
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2023-12-28 04:15:37 +0800

    experiment

---
+++ projecttwo/mrtpay/src/Bank.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Bank.css';
+ 
+const Bank = () => {
+    return (
+        <div>
+            <h1> WELCOME!
+            </h1>
+            <input placeholder='First Name'/>
+            <button >Submit</button>
+        </div>
+    )
+}
+ 
+export default Bank;
