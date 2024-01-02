88ed1132cbbc1881973e5fdb2a4d7f18a09e42b0
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-01-03 01:51:21 +0800

    test

--- projecttwo/mrtpay/src/App.tsx
+++ projecttwo/mrtpay/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 import MrtMap from './components/mrtMap';
+<<<<<<< Updated upstream
+=======
+// import NavBar from './components/navBar';
+import trainLogo from './trainLogo.png';
+>>>>>>> Stashed changes
+
 
 function MRTPay() {
     const UID = 123456789;
@@ -12,6 +18,7 @@ function MRTPay() {
     const formattedDate = `${currentDate.getDate()} ${currentDate.toLocaleString('default', { month: 'short' })}`;
 
     return (
+<<<<<<< Updated upstream
         <div className="map-container">
             <MrtMap/>
             <div className="objects">
@@ -22,6 +29,29 @@ function MRTPay() {
                     <div className="font-sans text-6xl font-black">{loc}</div>
                 </div>
                 <div className="CARD w-96 h-56 bg-white rounded-lg drop-shadow-lg fixed bottom-5 right-5 ">
+=======
+        <div className="bg-color relative h-screen">
+            <div  className="map-container w-4/6 h-fit bg-white rounded-lg drop-shadow-lg absolute right-0 m-6 ">
+                <MrtMap/>
+            </div>
+            <div className="flex z-0">
+                <div className="NAVBAR w-3/12 h-svh bg-teal-900 rounded-r-2xl drop-shadow-lg "></div>
+                <div className="flex flex-col">
+                    <div className="topBar h-56 w-3/12 bg-teal-950 rounded-tr-2xl absolute left-0 top-0 flex justify-center items-center">
+                        <img src={trainLogo} className="absolute left-0 top-2" alt="train logo"></img>
+                        <div className= "LOCATION flex flex-wrap flex-col place-items-center"> 
+                            <div className="currentLoc font-sans text-2xl text-yellow-600"> You are currently in</div>
+                            <div className="font-sans text-5xl font-black text-white italic">{loc}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="UI flex flex-col absolute left-5 grid gap-4 z-10">
+                {/* <div className="LOGO font-sans text-8xl font-bold text-black absolute right-48 top-6">MRT</div>
+                <div className="LOGO font-sans text-xl absolute right-60 top-28">put LOGO here</div> */}
+                <input className="UID w-96 h-14 bg-teal-50 rounded-lg drop-shadow-lg" placeholder="UID"></input>
+                <div className="CARD w-96 h-56 bg-teal-50 rounded-lg drop-shadow-lg">
+>>>>>>> Stashed changes
                     <div className="bg-gradient-to-r from-indigo-500"></div>
                     <div className="firstLine p-3 flex justify-between pb-0">
                         <div className='text1 font-semibold'>UID {UID}</div>
