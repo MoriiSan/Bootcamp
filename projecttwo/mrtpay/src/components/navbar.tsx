cb33c69f12a534fac927a67a1aba4d042b413857
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-01-18 21:15:25 +0800

    update logout

--- projecttwo/mrtpay/src/components/navbar.tsx
+++ projecttwo/mrtpay/src/components/navbar.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsTrainFront } from 'react-icons/bs';
-import {useAuth} from '../middlewares/authentication';
+import { useAuth } from '../middlewares/authentication';
 import './navbar.css'
 import { NavLink, useNavigate } from 'react-router-dom';
 
@@ -9,13 +9,21 @@ const Navbar = () => {
     const [isCardSelected, setIsCardSelected] = useState(false);
     const [isStationSelected, setIsStationSelected] = useState(false);
 
-    const handleCardClick = () => {setIsCardSelected(true);setIsStationSelected(false);};   
-    const handleStationClick = () => {setIsCardSelected(false);setIsStationSelected(true);};
-    const handleLogoClick = () => {setIsCardSelected(false);setIsStationSelected(false);
-            navigate('/admin');};
-    
+    const handleCardClick = () => { setIsCardSelected(true); setIsStationSelected(false); };
+    const handleStationClick = () => { setIsCardSelected(false); setIsStationSelected(true); };
+    const handleLogoClick = () => {
+        setIsCardSelected(false); setIsStationSelected(false);
+        navigate('/admin');
+    };
 
-    const {logout} = useAuth();
+    const { logout, isAuthenticated } = useAuth();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate('/');
+        }
+    }, [isAuthenticated, navigate]);
+
     const handleLogout = async () => {
         try {
             await logout();
@@ -23,29 +31,29 @@ const Navbar = () => {
             console.error('Error during logout:', error);
         }
     };
-    
+
     return (
         <div>
             <div className="navbar">
-                <div className="logo-layout" onClick={handleLogoClick}>                  
+                <div className="logo-layout" onClick={handleLogoClick}>
                     <BsTrainFront size={30}></BsTrainFront>
-                    <div className="logo-name"> MRT</div>     
+                    <div className="logo-name"> MRT</div>
                 </div>
-                    <div className="options">
-                        <div className={`cards ${isCardSelected ? 'selected' : ''}`}      
-                                onClick={handleCardClick}
-                                ><NavLink to="uid">Cards</NavLink>                               
-                                </div>
-                        <div className={`stations ${isStationSelected ? 'selected' : ''}`}
-                                onClick={handleStationClick}
-                                ><NavLink to="stations">Stations</NavLink>
-                                </div>
-                        </div>
+                <div className="options">
+                    <div className={`cards ${isCardSelected ? 'selected' : ''}`}
+                        onClick={handleCardClick}
+                    ><NavLink to="uid">Cards</NavLink>
+                    </div>
+                    <div className={`stations ${isStationSelected ? 'selected' : ''}`}
+                        onClick={handleStationClick}
+                    ><NavLink to="stations">Stations</NavLink>
+                    </div>
+                </div>
                 <div className="logout"
-                        onClick={handleLogout} >Logout</div>
+                    onClick={handleLogout} >Logout</div>
             </div>
         </div>
-      );
-    }
+    );
+}
 
 export default Navbar;
