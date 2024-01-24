758bd6d232dd0d339fde87ceadcb6ac3a18eb213
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-01-24 22:11:46 +0800

    update - fare BE

--- projecttwo/mrtpay/src/components/station/stations.tsx
+++ projecttwo/mrtpay/src/components/station/stations.tsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import './stations.css'
 
 const Stations = () => {
+    const [fare, setFare] = useState<Fare[]>([]);
+    interface Fare {
+        fare: number;
+    }
+
+    const fetchFare = async () => {
+        try {
+            const response = await fetch(`http://localhost:8090/adminConfigs/fare`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                const fetchedFare = await response.json();
+                setFare(fetchedFare);
+            } else {
+                console.error('Failed to fetch fare');
+            }
+        } catch (error) {
+            console.error('Error fetching fare:', error);
+        }
+    };
+
     return (
         <div>
             <div className="main-row">
