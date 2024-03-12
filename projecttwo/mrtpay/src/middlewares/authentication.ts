96c3504a42508be0c0f80c3bd389fabfadc4c620
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-03-13 02:56:47 +0800

    update session token

--- projecttwo/mrtpay/src/middlewares/authentication.ts
+++ projecttwo/mrtpay/src/middlewares/authentication.ts
@@ -1,23 +1,53 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export const useAuth = () => {
     const navigate = useNavigate();
+    const [dbToken, setDbToken] = useState('')
+
+
 
     useEffect(() => {
         const sessionToken = localStorage.getItem('TICKETING-AUTH') ?? '';
 
         if (!sessionToken) {
-            navigate('/');
+            if (sessionToken !== dbToken) {
+                localStorage.removeItem('TICKETING-AUTH');
+                navigate('/');
+            }
         }
+
         // console.log("t", sessionToken)
 
         try {
-            const decoded = jwtDecode(sessionToken);
+            const decoded = jwtDecode<JwtPayload>(sessionToken);
+            const userId = decoded.userId;
             // const decoded = jwt.verify(sessionToken, 'a1s2d3f4g5h6j7k8l90') as JwtPayload;
 
+            const fetchUser = async () => {
+                try {
+                    const response = await fetch(`${process.env.REACT_APP_URL}users/${userId}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                    const user = await response.json();
+                    if (response.ok) {
+                        setDbToken(user.authentication.sessionToken)
+                        console.log(user)
+                    } else {
+                        console.error('Failed to fetch user');
+                    }
+                } catch (error) {
+                    console.error('Error fetching user:', error);
+                }
+            };
+
+            // console.log(fetchUser)
+            fetchUser();
 
             const currentTimestamp = Math.floor(Date.now() / 3600);
             if (decoded.exp && decoded.exp < currentTimestamp) {
@@ -32,6 +62,8 @@ export const useAuth = () => {
             localStorage.removeItem('TICKETING-AUTH');
             navigate('/');
         }
+
+
     }, [navigate]);
 
     const login = (token: string) => {
